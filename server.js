0d8f69b7622f9658b1d7c83ddb179ac296032250
nyblad <nyblad@hotmail.com>
2020-02-28 11:55:57 +0100

    Chaged to findOneAndUpdate on todo put

--- server.js
+++ server.js
@@ -230,8 +230,8 @@ app.put('/todos/:id', async (req, res) => {
   const { id } = req.params
   try {
     //Sucess
-    // await Todo.findOneAndUpdate({ '_id': id }, req.body, { new: true })
-    await Todo.updateOne({ '_id': id }, req.body)
+    await Todo.findOneAndUpdate({ '_id': id }, req.body, { new: true })
+    // await Todo.updateOne({ '_id': id }, req.body)
     res.status(201).json()
   } catch (err) {
     // Failed
